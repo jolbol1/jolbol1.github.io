e7e623c8c7e649d88b3463102b8b1391ae7f51a6
james <github@jamesshopland.com>
2022-01-21 13:43:35 +0000

    Add gatsby manifest for icons

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,5 +16,19 @@ module.exports = {
       },
       __key: "images",
     },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `James Shopland`,
+        short_name: `JamesShopland`,
+        description: `James Shopland. Software Developer. I use this site as a test bench for new frameworks and to improve my skills in web design.`,
+        lang: `en`,
+        start_url: `/`,
+        background_color: `rgb(23, 23, 49)`,
+        theme_color: `#fff`,
+        display: `standalone`,
+        icon: 'src/images/JSLogo.png',
+      },
+    },
   ],
 };
