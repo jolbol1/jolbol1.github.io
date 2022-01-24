205d7830eeb7c8cd692ef53860c90368c3b60fd9
james <github@jamesshopland.com>
2022-01-24 20:46:00 +0000

    Add fonts from fontSource

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,4 +1,12 @@
 import './src/styles/global.css'
+import "@fontsource/montserrat/400.css"
+import "@fontsource/montserrat/500.css"
+import "@fontsource/montserrat/600.css"
+import "@fontsource/montserrat/700.css" 
+import "@fontsource/montserrat/800.css"
+import "@fontsource/montserrat/900.css"
+
+
 
 
 export function onInitialClientRender() { window.scrollTo(0, 0) }
