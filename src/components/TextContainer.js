5971ba60351b40997e7df241fcfccec278f60640
Jolbol1 <github@jamesshopland.com>
2022-04-30 17:53:52 +0100

    fix typo

--- src/components/TextContainer.js
+++ src/components/TextContainer.js
@@ -10,7 +10,7 @@ export const TextContainer = () => {
         </h4>
         <p className="text-justify text-base md:text-lg	">
           I am a software engineer currently working for a leading systems, engineering and technology company in the Cloud Technologies department.
-          Previosuly worked for a medium sized finance firm, developing in house apps to improve workflows. 
+          Previously worked for a medium sized finance firm, developing in house apps to improve workflows. 
           I have skills in developing CRUD apps with .NET, Building OfficeJS add-ins and full-stack web design.
         </p>
         <br />
@@ -20,7 +20,7 @@ export const TextContainer = () => {
         <p className="text-justify text-base md:text-lg">
           I started coding over 10 years ago in Java, developing plugins for
           games I liked. Since then I have always had an interest in all things
-          tech, from microcontrollers to full-stack development. The combination
+          tech, from micro-controllers to full-stack development. The combination
           of problem solving and creativity continue to fuel my passion in the
           rapidly changing field of technology.
         </p>
