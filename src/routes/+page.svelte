e214179d798332586d790e1af6c698ff97d18f03
jolbol1 <github@jamesshopland.com>
2022-08-16 00:06:53 +0100

    🐛 fix(svelte): migrate to new svelte routes version

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,3 @@
-<script context="module" lang="ts">
-	export const prerender = true;
-</script>
-
 <script lang="ts">
 	import IntroSection from '../components/IntroSection.svelte';
 	import AboutSection from '../components/AboutSection.svelte';
