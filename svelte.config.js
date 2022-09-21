14fa2c550928ee95a580e9ec46360a5c9f4912eb
jolbol1 <github@jamesshopland.com>
2022-09-21 21:47:25 +0100

    ♻️ refactor svelete prerender changes and misc

--- svelte.config.js
+++ svelte.config.js
@@ -13,14 +13,7 @@ const config = {
 	],
 
 	kit: {
-		adapter: adapter(),
-		prerender: {
-			default: true
-		},
-		// Override http methods in the Todo forms
-		methodOverride: {
-			allowed: ['PATCH', 'DELETE']
-		}
+		adapter: adapter()
 	}
 };
 
